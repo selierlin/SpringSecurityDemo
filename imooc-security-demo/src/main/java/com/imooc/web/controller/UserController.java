cabfe6adbfb9804fb424497fe1c674fe03bede42
theliang <10780_glb@qq.com>
2019-03-30 17:49:59 +0800

    编写用户详情服务： @PathVariable 映射url片段到Java方法的参数 在url声明中使用正则表达式

--- imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.imooc.web.controller;
 
 import com.imooc.dto.User;
+import com.imooc.dto.UserQueryCondition;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,13 +17,67 @@ import java.util.List;
 public class UserController {
 	
 	@GetMapping("/user")
-	public List<User> query(@RequestParam String username){
-		System.out.println(username);
+	public List<User> query(UserQueryCondition condition
+			, @PageableDefault(page = 2,size = 10,sort="username,desc") Pageable pageable){
+		//System.out.println(userQueryCondition);
+		System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+		System.out.println(ReflectionToStringBuilder.toString(pageable, ToStringStyle.MULTI_LINE_STYLE));
 		ArrayList<User> users = new ArrayList<>();
 		users.add(new User());
 		users.add(new User());
 		users.add(new User());
 		return users;
 	}
+
+	@GetMapping("/user/{id:\\d+}")//id:\d+表示正则匹配数字
+	public User getInfo(@PathVariable String id) {
+		System.out.println("进入getInfo服务");
+		User user = new User();
+		user.setUsername("tom");
+		return user;
+	}
+
+	/*@PostMapping("/regist")
+	public void regist(User user, HttpServletRequest request) {
+
+		//不管是注册用户还是绑定用户，都会拿到一个用户唯一标识。
+		String userId = user.getUsername();
+		providerSignInUtils.doPostSignUp(userId, new ServletWebRequest(request));
+	}
+
+	@GetMapping("/me")
+	public Object getCurrentUser(@AuthenticationPrincipal UserDetails user) {
+		return user;
+	}
+
+	@PostMapping
+	@ApiOperation(value = "创建用户")
+	public User create(@Valid @RequestBody User user) {
+
+		System.out.println(user.getId());
+		System.out.println(user.getUsername());
+		System.out.println(user.getPassword());
+		System.out.println(user.getBirthday());
+
+		user.setId("1");
+		return user;
+	}
+
+	@PutMapping("/{id:\\d+}")
+	public User update(@Valid @RequestBody User user, BindingResult errors) {
+
+		System.out.println(user.getId());
+		System.out.println(user.getUsername());
+		System.out.println(user.getPassword());
+		System.out.println(user.getBirthday());
+
+		user.setId("1");
+		return user;
+	}
+
+	@DeleteMapping("/{id:\\d+}")
+	public void delete(@PathVariable String id) {
+		System.out.println(id);
+	}*/
 
 }
--- imooc-security-demo/src/test/java/com/imooc/web/controller/UserControllerTest.java
+++ imooc-security-demo/src/test/java/com/imooc/web/controller/UserControllerTest.java
@@ -35,11 +35,35 @@ public class UserControllerTest {
     @Test
     public void whenQuerySuccess() throws Exception {
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/user")
-                        .param("username","传入的username")
+                MockMvcRequestBuilders.get("/user")//模拟请求/user地址
+                        //UserQueryCondition 相关参数：
+                        .param("username", "传入的username")
+                        .param("age", "22")
+                        .param("ageTo", "30")
+                        //Pageable相关参数：
+                        .param("size", "15").param("page", "3")
+                        .param("sort", "age,desc").param("sort", "username,asc")
                         .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isOk())//期望结果返回200
-                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));//期望返回结果长度为3
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));//期望返回结果的数组长度为3
+    }
+
+    @Test
+    public void whenGetInfoSuccess() throws Exception {
+        String result = mockMvc.perform(MockMvcRequestBuilders.get("/user/1")//模拟请求/user/1地址
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())//期望结果返回200
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("tom"))//期望返回的Json中username为tom
+                .andReturn().getResponse().getContentAsString();//获取返回值
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenGetInfoFail() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/a")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError());//期望响应的状态码为4xx
     }
 
 }
