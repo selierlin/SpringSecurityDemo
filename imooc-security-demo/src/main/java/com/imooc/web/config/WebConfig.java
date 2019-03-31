53bb99aa73220bcd36045cb8708603219ca30ba1
theliang <10780_glb@qq.com>
2019-03-31 18:03:24 +0800

    添加拦截器（Interceptor）TimeInterceptor

--- imooc-security-demo/src/main/java/com/imooc/web/config/WebConfig.java
+++ imooc-security-demo/src/main/java/com/imooc/web/config/WebConfig.java
@@ -1,9 +1,12 @@
 package com.imooc.web.config;
 
 import com.imooc.web.filter.TimeFilter;
+import com.imooc.web.interceptor.TimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.ArrayList;
@@ -11,7 +14,14 @@ import java.util.List;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
-	
+	@Autowired
+	private TimeInterceptor timeInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(timeInterceptor);
+	}
+
 	@Bean
 	public FilterRegistrationBean timeFilter() {
 		
