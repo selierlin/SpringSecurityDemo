da913b2c2700b15a2b72e3ffcc55f5a2a7734887
theliang <10780_glb@qq.com>
2019-04-01 11:20:37 +0800

    使用DeferredResult异步处理Rest服务

--- imooc-security-demo/src/main/java/com/imooc/web/async/AsyncController.java
+++ imooc-security-demo/src/main/java/com/imooc/web/async/AsyncController.java
@@ -1,29 +1,36 @@
 package com.imooc.web.async;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.Callable;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @RestController
 public class AsyncController {
+    @Autowired
+    private MockQueue mockQueue;
 
+    @Autowired
+    private DeferredResultHolder deferredResultHolder;
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/order")
-    public Callable<String> order() {
+    public DeferredResult<String> order() throws Exception {
         logger.info("主线程开始");
-        //Thread.sleep(1000);
-        Callable<String> result = () -> {
-            logger.info("副线程开始");
-            Thread.sleep(1000);
-            logger.info("副线程返回");
-            return "success";
-        };
-        logger.info("主线程返回");
+
+        String orderNumber = RandomStringUtils.randomNumeric(8);
+        logger.info("orderNumber="+orderNumber);
+        mockQueue.setPlaceOrder(orderNumber);
+
+        logger.info("=====28");
+        DeferredResult<String> result = new DeferredResult<>();//异步生成返回值
+        logger.info("=====29");
+        deferredResultHolder.getMap().put(orderNumber, result);
+        logger.info("等待返回结果");
         return result;
     }
 
