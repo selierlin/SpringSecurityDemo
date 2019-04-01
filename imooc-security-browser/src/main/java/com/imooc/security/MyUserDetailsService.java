33eac7f71f1726a80925b07342b1bc3ef0740243
theliang <10780_glb@qq.com>
2019-04-01 16:03:38 +0800

    处理密码加密解密

--- imooc-security-browser/src/main/java/com/imooc/security/MyUserDetailsService.java
+++ imooc-security-browser/src/main/java/com/imooc/security/MyUserDetailsService.java
@@ -2,12 +2,14 @@ package com.imooc.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,14 +18,19 @@ import java.util.List;
 public class MyUserDetailsService implements UserDetailsService {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		logger.info("表单登录用户名:" + username);
 		List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList("admin");
         //根据查找到的用户信息判断用户是否被冻结
-		return new User(username,"123456",
-                true,true,true,false,//是否可用、过期、凭证过期、账户锁定
+        String password = passwordEncoder.encode("123456");
+        logger.info("数据库密码是:"+password);
+        return new User(username,password,
+                true,true,true,true,//是否可用、过期、凭证过期、账户锁定
                 authorities);
 	}
 
--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.imooc.security.browser;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,5 +17,10 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()//对请求进行授权
                 .anyRequest()//针对所有请求
                 .authenticated();//需要身份认证
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
