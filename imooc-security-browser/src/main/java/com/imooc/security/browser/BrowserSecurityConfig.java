6d456373384284f5f20e1b3367bc70861716ab73
theliang <10780_glb@qq.com>
2019-04-01 15:42:42 +0800

    SpringSecurity基本配置

---
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.imooc.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()//登录页的主题为表单类型
+        //http.httpBasic()//登录页的主题为弹窗类型
+                .and()
+                .authorizeRequests()//对请求进行授权
+                .anyRequest()//针对所有请求
+                .authenticated();//需要身份认证
+    }
+}
