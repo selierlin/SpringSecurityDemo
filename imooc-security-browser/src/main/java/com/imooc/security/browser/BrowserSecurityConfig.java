98f03e3aada56bc4eefce868434bdeaab1964b25
theliang <10780_glb@qq.com>
2019-04-02 10:57:34 +0800

    自定义登录请求及跳转提示

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.imooc.security.browser;
 
+import com.imooc.security.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,15 +11,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()//登录页的主题为表单类型
         //http.httpBasic()//登录页的主题为弹窗类型
-                .loginPage("/imooc-signIn.html")//设置登录页面
+                .loginPage("/authentication/require")//设置登录页面
                 .loginProcessingUrl("/authentication/form")//登录请求处理地址
                 .and()
                 .authorizeRequests()//对请求进行授权
-                .antMatchers("/imooc-signIn.html").permitAll()//对此地址不拦截
+                .antMatchers("/authentication/require"
+                        ,securityProperties.getBrowser().getLoginPage()).permitAll()//对此地址不拦截
                 .anyRequest()//针对所有请求
                 .authenticated()//需要身份认证
                 .and().csrf().disable();//禁用CSRF
