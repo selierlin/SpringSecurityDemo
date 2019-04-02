b30d2e2c064ea26473927c0b7dd6d70079e1fb06
theliang <10780_glb@qq.com>
2019-04-02 10:20:32 +0800

    自定义登录页面

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -13,10 +13,14 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()//登录页的主题为表单类型
         //http.httpBasic()//登录页的主题为弹窗类型
+                .loginPage("/imooc-signIn.html")//设置登录页面
+                .loginProcessingUrl("/authentication/form")//登录请求处理地址
                 .and()
                 .authorizeRequests()//对请求进行授权
+                .antMatchers("/imooc-signIn.html").permitAll()//对此地址不拦截
                 .anyRequest()//针对所有请求
-                .authenticated();//需要身份认证
+                .authenticated()//需要身份认证
+                .and().csrf().disable();//禁用CSRF
     }
 
     @Bean
